638e1ffa6451faacb3bd5adeceb70c3007ce8dd7
ayaikg <ayaka777342@yahoo.co.jp>
2023-10-31 22:49:31 +0900

    posts_controller設定

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :advice)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
