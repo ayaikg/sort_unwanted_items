67d5e39fec784ca15bea3aa17bdc595084698870
ayaikg <ayaka777342@yahoo.co.jp>
2023-10-31 22:33:39 +0900

    postsテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_081758) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_132607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_081758) do
     t.index ["item_id"], name: "index_notifications_on_item_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.text "advice"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_posts_on_item_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -71,4 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_081758) do
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "notifications", "items"
+  add_foreign_key "posts", "items"
+  add_foreign_key "posts", "users"
 end
