9eff92eee8e023aca39c4ebe1bcec152e84534ca
ayaikg <ayaka777342@yahoo.co.jp>
2023-11-09 17:11:53 +0900

    マイページ用のコントローラー設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
   end
+
+  def show; end
 
   def create
     @user = User.new(user_params)
@@ -14,7 +17,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.name)
+    else
+      redirect_to edit_user_path(current_user.name)
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find_by(name: params[:name])
+    redirect_to user_path(@user) unless current_user?(@user)
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :avatar, :introduction, :name)
