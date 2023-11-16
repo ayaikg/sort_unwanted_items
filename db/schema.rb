30640e4bb21d4c9d7d5a66a60d80f3dcf3419c41
ayaikg <ayaka777342@yahoo.co.jp>
2023-11-16 21:21:17 +0900

    declutteringsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_042226) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_121355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_042226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "declutterings", force: :cascade do |t|
+    t.integer "goal_amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_declutterings_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -90,6 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_042226) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "declutterings", "users"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "likes", "posts"
