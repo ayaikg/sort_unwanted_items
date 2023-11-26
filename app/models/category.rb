155bf18ddac448e2e9e03aea5e6b00d6be1d27cf
ayaikg <ayaka777342@yahoo.co.jp>
2023-11-26 21:19:17 +0900

    一部のdependent: destroyオプションを削除

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   mount_uploader :icon, IconUploader
   belongs_to :user
-  has_many :items, dependent: :destroy
+  has_many :items
 
   validates :title, presence: true
 end
