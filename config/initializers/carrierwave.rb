2d435ac706fd4fc2b31ce32f2d0e86f8255bcf19
ayaikg <ayaka777342@yahoo.co.jp>
2023-12-01 12:49:02 +0900

    カテゴリ削除時にdisposal_methodの状態によってアイテムの挙動を変更、タイプミス修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,9 +10,9 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id]
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
-      region: 'ap-northeast-1'
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1',
       path_style: true
     }
   else
