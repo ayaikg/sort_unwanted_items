ae8c6a721bb4a7a93d9b20b320a4f93e020b5821
ayaikg <ayaka777342@yahoo.co.jp>
2023-12-19 14:22:04 +0900

    タブ切り替えで選択したタブのまま表示

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -4,21 +4,40 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["status", "content"]
 
+  connect() {
+    this.showCurrentTab()
+  }
+
   status(event) {
     const status = this.statusTargets
     const current = event.currentTarget
     const currentIndex = status.indexOf(current)
+    this.saveCurrentTab(currentIndex)
+    this.showTab(currentIndex)
+  }
+
+  saveCurrentTab(index) {
+    localStorage.setItem('currentTab', index)
+  }
+
+  showCurrentTab() {
+    const savedIndex = localStorage.getItem('currentTab') || 0
+    this.showTab(savedIndex)
+  }
+
+  showTab(index) {
+    const status = this.statusTargets
     const contents = this.contentTargets
-  
-    status.forEach((status, index) => {
-      if(status == current) {
+
+    status.forEach((status, idx) => {
+      if(idx == index) {
         status.classList.remove("not-active")
         status.classList.add("tab-active")
-        contents[index].classList.remove("hidden")
+        contents[idx].classList.remove("hidden")
       } else {
         status.classList.remove("tab-active")
         status.classList.add("not-active")
-        contents[index].classList.add("hidden")
+        contents[idx].classList.add("hidden")
       }
     })
   }
