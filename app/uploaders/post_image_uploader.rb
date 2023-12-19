824abe5f2264d7effd280852bf80296602b0d08e
ayaikg <ayaka777342@yahoo.co.jp>
2023-12-19 23:19:52 +0900

    画像のリサイズ変更と投稿詳細のデザイン変更

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -33,7 +33,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fill: [300, 200, "Center"]
+  process resize_to_fill: [300, 300, "Center"]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
