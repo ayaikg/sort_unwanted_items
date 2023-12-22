59f9ff382e95c8841feef3ae22e2cf07e71a18b3
ayaikg <ayaka777342@yahoo.co.jp>
2023-12-22 10:39:18 +0900

    ユーザ編集時のエラーを修正、id名を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, allow_nil: true
   validates :name, presence: true
 
   enum role: { general: 0, admin: 1 }
