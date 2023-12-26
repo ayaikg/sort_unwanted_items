c589bf8a6dd56f005fcc524ff06067f402679ca1
ayaikg <ayaka777342@yahoo.co.jp>
2023-12-26 14:48:18 +0900

    rubocopエラーの修正

--- lib/line_client.rb
+++ lib/line_client.rb
@@ -2,7 +2,7 @@ module LineClient
   module_function
 
   def line_bot_client
-    client = Line::Bot::Client.new do |config|
+    Line::Bot::Client.new do |config|
       config.channel_secret = Rails.application.credentials.dig(:line, :secret)
       config.channel_token = Rails.application.credentials.dig(:line, :channel_token)
     end
