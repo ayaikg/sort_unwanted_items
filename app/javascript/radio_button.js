cd68139fad268d0fbdec3fa95cb906363dd790d5
ayaikg <ayaka777342@yahoo.co.jp>
2024-01-13 22:44:35 +0900

    投稿フォームのアイテム選択欄のデザインを変更

--- app/javascript/radio_button.js
+++ app/javascript/radio_button.js
@@ -2,8 +2,7 @@ document.addEventListener('turbo:load', function() {
   // 「投稿するアイテム」の下に表示するための要素を作成
   const itemInfo = document.createElement('p');
   itemInfo.id = 'item-info'; // この要素を操作しやすくするためにIDを付与
-  itemInfo.classList.add('mb-2', 'text-center'); // スタイルを適用
-  itemInfo.textContent = 'アイテムを選択してください'; // 初期テキスト
+  itemInfo.classList.add('m-auto', 'w-3/4', 'border-b', 'border-secondary', 'text-lg', 'pb-2', 'text-center'); // スタイルを適用
 
   // 「投稿するアイテム」のpタグを取得して、その後ろにitemInfoを挿入
   const itemLabel = document.querySelector('#title');
@@ -13,6 +12,12 @@ document.addEventListener('turbo:load', function() {
 
   // radioボタンの要素を取得する
   const radioButtons = document.querySelectorAll('input[type="radio"][name="post[item_id]"]');
+
+  if (radioButtons.length > 0) {
+    itemInfo.textContent = 'アイテムを選択してください';
+  } else {
+    itemInfo.textContent = 'アイテムがありません。';
+  }
 
   const updateItemInfo = (radio) => {
     // ラジオボタンが選択されたときの処理
